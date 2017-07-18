1a61c976a33d7dc050d21902427614525ee7c9fc
Albert Liu <albert6liu@gmail.com>
2017-07-18 02:55:17 +0000

    javascript_motto_cycling_and_new_social_media_buttons

--- web/assets/scripts/animation-controls.js
+++ web/assets/scripts/animation-controls.js
@@ -2,11 +2,23 @@
 
 
 // init global vars
-var Mottolist = [];
+var displayArea;
+var Mottolist = [   `Experience Local Classes`,
+                    `Find Your Passion`,
+                    `You Want It? We've Got It`,
+                    `Yolo Right?`,
+                    'Volunteer As Tribute!',
+                    `Find Hot Single Classes Near You!`,
+                    `Take The Red Pill Neo!`,
+                    `Don't Worry, We Won't Bite`,
+                    `Just Keep Trying Till You Run Out of Cake`,
+                    `Are YOU Bad enough to Rescue the President?`,
+                    `Revolutionize Your Hobbies`
+                    ];
 
 function init(){
-    // populate Mottolist
-    Mottolist = (document.getElementsByClassName("motto"))
+    // find scrolling-text div
+    displayArea = document.getElementById("changing-motto")
     
 }
 
@@ -14,29 +26,20 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function mottoIn(index){
-    Mottolist[index].classList.remove("hidden");
-    Mottolist[index].classList.add("entering");
-    Mottolist[index].classList.remove("leaving");
-    
-}
-
-function mottoOut(index){
-    Mottolist[index].classList.remove("entering");
-    Mottolist[index].classList.add("leaving");
-}
-
 async function cycleMottos(){
     var index = 0;
     while(true){
-        await sleep(4000);
-        mottoOut(index);
+        displayArea.classList.remove("entering");
+        displayArea.classList.add("leaving");
         index += 1;
         if (index == Mottolist.length){
             index = 0;
         }
         await sleep(500);
-        mottoIn(index);
+        displayArea.innerHTML = Mottolist[index];
+        displayArea.classList.remove("leaving");
+        displayArea.classList.add("entering");
+        await sleep(5000);
     }
 };
 
