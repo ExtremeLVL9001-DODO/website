2de2503f9e7db50b94f0b4e3ac6426fc31f054e6
Albert Liu <albert6liu@gmail.com>
2017-07-23 03:20:28 +0000

    new animations, email validation, and signup completion message

--- web/assets/scripts/animation-controls.js
+++ web/assets/scripts/animation-controls.js
@@ -15,6 +15,7 @@ var Mottolist = [   `Experience Local Classes`,
                     `Are YOU Bad enough to Rescue the President?`,
                     `Revolutionize Your Hobbies`
                     ];
+var SignupMsg = "Thank You For Helping Us";
 
 function init(){
     // find scrolling-text div
@@ -56,4 +57,49 @@ window.onload = function(){
     
     init();
     cycleMottos();
+};
+
+// SIGNUP ANIMATIONS
+async function signupError(errorString,errorHighlight){
+    var errorParent = document.getElementById("signup");
+    
+    if (errorHighlight == -1){ // -1 is successful send, delete errormessage
+        var oldError = document.getElementById("errorOutput");
+        errorParent.removeChild(oldError);
+        return
+    }
+    
+    // set focus to error portion
+    errorParent.childNodes[((errorHighlight * 2) + 1)].focus();
+    
+    
+    var oldError = document.getElementById("errorOutput");
+    if (oldError != null){ //previous error exists
+        oldError.classList.remove("errorIn");
+        oldError.classList.add("errorOut");
+        await sleep(300);
+        oldError.innerHTML = errorString;
+        oldError.classList.remove("errorOut");
+        oldError.classList.add("errorIn");
+    }
+    else{
+    // first error, so create new node
+    var errorOutput = document.createElement("p");
+    errorOutput.innerHTML = errorString;
+    errorOutput.classList.add("text");
+    errorOutput.classList.add("errorIn");
+    errorOutput.setAttribute("id","errorOutput");
+    
+    errorParent.appendChild(errorOutput);
+    }
+};
+
+async function signupSuccess(){
+    var signupForm = document.getElementById("signup");
+    signupForm.classList.add("formOut");
+    await sleep(1100);
+    signupForm.innerHTML = SignupMsg;
+    signupForm.classList.remove("formOut")
+    signupForm.classList.add("formText")
+    signupForm.classList.add("formIn")
 };
